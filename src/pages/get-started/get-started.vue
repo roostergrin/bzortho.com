3ad294720199d3739035c6e08c601098da0ed5f5
mattlaguardia <matthewlaguardia@gmail.com>
2017-12-27 11:06:21 -0800

    feat(custom-getting-started-first-visit): creates component and adds styling

--- src/pages/get-started/get-started.vue
+++ src/pages/get-started/get-started.vue
@@ -4,6 +4,7 @@
 import Loader from 'components/loader/loader'
 import Hero from 'components/hero/hero'
 import Icon from 'components/icon/icon'
+import CustomGetStartedFirstVisit from 'components/custom/custom-get-started-first-visit/custom-get-started-first-visit'
 import CustomGetStartedServices from 'components/custom/custom-get-started-services/custom-get-started-services'
 import BlockTextBox from 'components/block/block-text-box/block-text-box'
 import CustomPatientForms from 'components/custom/custom-patient-forms/custom-patient-forms'
@@ -24,7 +25,8 @@ export default {
     Icon,
     CustomGetStartedServices,
     BlockTextBox,
-    CustomPatientForms
+    CustomPatientForms,
+    CustomGetStartedFirstVisit
   }
 }
 </script>
