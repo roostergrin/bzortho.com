4242ab0089da26347d2cdf1ef81a0eb60f54a776
luketevers <luketevers@gmail.com>
2017-12-26 16:00:47 -0800

    added changes to getting started page at the bottom of the page

--- src/pages/get-started/get-started.vue
+++ src/pages/get-started/get-started.vue
@@ -2,8 +2,11 @@
 
 <script>
 import Loader from 'components/loader/loader'
+import NavTrigger from 'components/nav/custom-nav-trigger/custom-nav-trigger'
 import Hero from 'components/hero/hero'
 import Icon from 'components/icon/icon'
+import BlockBox from 'components/block/block-text-box/block-text-box'
+import ImageButton from 'components/custom/custom-image-button/custom-image-button'
 
 export default {
   name: 'get-started',
@@ -17,8 +20,11 @@ export default {
   },
   components: {
     Loader,
+    NavTrigger,
     Hero,
-    Icon
+    Icon,
+    BlockBox,
+    ImageButton
   }
 }
 </script>
