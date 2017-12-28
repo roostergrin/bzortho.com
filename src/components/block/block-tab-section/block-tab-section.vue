d22acc528ec90340ecf2324ac1bf34c4f675cc25
mattlaguardia <matthewlaguardia@gmail.com>
2017-12-28 14:35:05 -0800

    made changes

--- src/components/block/block-tab-section/block-tab-section.vue
+++ src/components/block/block-tab-section/block-tab-section.vue
@@ -5,7 +5,7 @@ export default {
   props: ['props'],
   data () {
     return {
-      body: 'Traditional metal braces are the most common type of braces and are more comfortable today than ever before. Made of high-grade stainless steel, metal braces straighten your teeth using metal brackets and archwires. With metal braces, you have the option of adding colored elastics (rubber bands) for a more unique and colorful smile.',
+      // body: 'Traditional metal braces are the most common type of braces and are more comfortable today than ever before. Made of high-grade stainless steel, metal braces straighten your teeth using metal brackets and archwires. With metal braces, you have the option of adding colored elastics (rubber bands) for a more unique and colorful smile.',
       i: 0
     }
   },
@@ -16,7 +16,7 @@ export default {
     }
   },
   created () {
-    this.switchTab(0, 'Traditional metal braces are the most common type of braces and are more comfortable today than ever before. Made of high-grade stainless steel, metal braces straighten your teeth using metal brackets and archwires. With metal braces, you have the option of adding colored elastics (rubber bands) for a more unique and colorful smile.')
+    this.switchTab(0, '<p>Traditional metal braces are the most common type of braces and are more comfortable today than ever before. Made of high-grade stainless steel, metal braces straighten your teeth using metal brackets and archwires. With metal braces, you have the option of adding colored elastics (rubber bands) for a more unique and colorful smile.</p><img src="http://zebrick.rgwplogin.com/wp-content/uploads/treatments-3.jpg" />')
   }
 }
 </script>
