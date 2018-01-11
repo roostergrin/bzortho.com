702562ca51910fad18ee672ee3f3ab973b4f817c
mattlaguardia <matthewlaguardia@gmail.com>
2018-01-10 18:53:53 -0800

    feat(new team-section): adds new team section for doc to checkout

--- src/components/custom/custom-about-us-team/custom-about-us-team.vue
+++ src/components/custom/custom-about-us-team/custom-about-us-team.vue
@@ -2,11 +2,13 @@
 
 <script>
 import SliderAboutUsTeam from 'components/slider/slider-about-us-team/slider-about-us-team'
+import GridTeam from 'components/grid/grid-team/grid-team'
 
 export default {
   props: ['props'],
   components: {
-    SliderAboutUsTeam
+    SliderAboutUsTeam,
+    GridTeam
   }
 }
 </script>
---
+++ src/components/grid/grid-team/grid-team.vue
@@ -0,0 +1,17 @@
+<template lang="pug" src="./grid-team.pug"></template>
+
+<script>
+export default {
+  props: ['props'],
+  data () {
+    return {
+      active: ''
+    }
+  },
+  methods: {
+    mouseOver (i) {
+      this.active === i ? this.active = '' : this.active = i
+    }
+  }
+}
+</script>
