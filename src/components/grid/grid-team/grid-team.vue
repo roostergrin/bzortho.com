2b1ad31656dcb90bcdc24db99fd7c850b9399cd5
mattlaguardia <matthewlaguardia@gmail.com>
2018-01-11 10:54:31 -0800

    updates build

--- src/components/grid/grid-team/grid-team.vue
+++ src/components/grid/grid-team/grid-team.vue
@@ -1,17 +1,31 @@
 <template lang="pug" src="./grid-team.pug"></template>
 
 <script>
+import ModalTeamBio from 'components/modal/modal-team-bio/modal-team-bio'
+
 export default {
   props: ['props'],
   data () {
     return {
-      active: ''
+      active: '',
+      modal: '',
+      show: false
     }
   },
   methods: {
     mouseOver (i) {
-      this.active === i ? this.active = '' : this.active = i
+      this.active = i
+    },
+    mouseLeave (i) {
+      this.active = ''
+    },
+    modalShow (info) {
+      this.modal = info
+      this.show = true
     }
+  },
+  components: {
+    ModalTeamBio
   }
 }
 </script>
---
+++ src/components/modal/modal-team-bio/modal-team-bio.vue
@@ -0,0 +1,12 @@
+<template lang="pug" src="./modal-team-bio.pug"></template>
+
+<script>
+import Icon from 'components/icon/icon'
+
+export default {
+  props: ['props'],
+  components: {
+    Icon
+  }
+}
+</script>
